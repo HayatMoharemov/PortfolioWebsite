ecfa6acd110b806b03b25abb52e72aab531466bf
Hayat Moharemov <h.moharemov@gmail.com>
2026-06-20 12:44:26 +0200

    BugFix: Dots no longer concentrated on the center of the screen on phones.Optimization: Script uses less RAM now

--- static/scripts/connectingDots.js
+++ static/scripts/connectingDots.js
@@ -1,104 +1,121 @@
 var canvasDots = function() {
+
     var canvas = document.querySelector('canvas'),
         ctx = canvas.getContext('2d'),
         colorDot = '#595959',
         color = '#8f8f8f';
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     canvas.style.display = 'block';
+
     ctx.fillStyle = colorDot;
     ctx.lineWidth = .1;
     ctx.strokeStyle = color;
 
     var mousePosition = {
-        x: 30 * canvas.width / 100,
-        y: 30 * canvas.height / 100
+        x: canvas.width / 2,
+        y: canvas.height / 2
     };
 
+    // Брой точки според площта на екрана
+    var area = canvas.width * canvas.height;
+
     var dots = {
-        nb: 600,
-        distance: 60,
-        d_radius: 100,
+        nb: Math.max(50, Math.floor(area / 5000)),
+        distance: window.innerWidth < 768 ? 40 : 60,
+        d_radius: window.innerWidth < 768 ? 70 : 100,
         array: []
     };
 
-    function Dot(){
+    function Dot() {
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
 
-        this.vx = -.5 + Math.random();
-        this.vy = -.5 + Math.random();
+        this.vx = -0.5 + Math.random();
+        this.vy = -0.5 + Math.random();
 
         this.radius = Math.random();
     }
 
-    Dot.prototype = {
-        create: function(){
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-            ctx.fill();
-        },
+    Dot.prototype.create = function() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fill();
+    };
 
-        animate: function(){
-            for(i = 0; i < dots.nb; i++){
+    Dot.prototype.animate = function() {
 
-                var dot = dots.array[i];
+        for (let i = 0; i < dots.nb; i++) {
 
-                if(dot.y < 0 || dot.y > canvas.height){
-                    dot.vx = dot.vx;
-                    dot.vy = - dot.vy;
-                }
-                else if(dot.x < 0 || dot.x > canvas.width){
-                    dot.vx = - dot.vx;
-                    dot.vy = dot.vy;
-                }
-                dot.x += dot.vx;
-                dot.y += dot.vy;
+            let dot = dots.array[i];
+
+            if (dot.y < 0 || dot.y > canvas.height) {
+                dot.vy = -dot.vy;
             }
-        },
 
-        line: function(){
-            for(i = 0; i < dots.nb; i++){
-                for(j = 0; j < dots.nb; j++){
-                    i_dot = dots.array[i];
-                    j_dot = dots.array[j];
+            if (dot.x < 0 || dot.x > canvas.width) {
+                dot.vx = -dot.vx;
+            }
 
-                    if((i_dot.x - j_dot.x) < dots.distance && (i_dot.y - j_dot.y) < dots.distance && (i_dot.x - j_dot.x) > - dots.distance && (i_dot.y - j_dot.y) > - dots.distance){
-                        if((i_dot.x - mousePosition.x) < dots.d_radius && (i_dot.y - mousePosition.y) < dots.d_radius && (i_dot.x - mousePosition.x) > - dots.d_radius && (i_dot.y - mousePosition.y) > - dots.d_radius){
-                            ctx.beginPath();
-                            ctx.moveTo(i_dot.x, i_dot.y);
-                            ctx.lineTo(j_dot.x, j_dot.y);
-                            ctx.stroke();
-                            ctx.closePath();
-                        }
+            dot.x += dot.vx;
+            dot.y += dot.vy;
+        }
+    };
+
+    Dot.prototype.line = function() {
+
+        for (let i = 0; i < dots.nb; i++) {
+
+            for (let j = i + 1; j < dots.nb; j++) {
+
+                let i_dot = dots.array[i];
+                let j_dot = dots.array[j];
+
+                if (
+                    Math.abs(i_dot.x - j_dot.x) < dots.distance &&
+                    Math.abs(i_dot.y - j_dot.y) < dots.distance
+                ) {
+
+                    if (
+                        Math.abs(i_dot.x - mousePosition.x) < dots.d_radius &&
+                        Math.abs(i_dot.y - mousePosition.y) < dots.d_radius
+                    ) {
+
+                        ctx.beginPath();
+                        ctx.moveTo(i_dot.x, i_dot.y);
+                        ctx.lineTo(j_dot.x, j_dot.y);
+                        ctx.stroke();
+                        ctx.closePath();
                     }
                 }
             }
         }
     };
 
-    function createDots(){
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        for(i = 0; i < dots.nb; i++){
-            dots.array.push(new Dot());
-            dot = dots.array[i];
-
-            dot.create();
-        }
-
-        dot.line();
-        dot.animate();
+    // Създаваме точките САМО ВЕДНЪЖ
+    for (let i = 0; i < dots.nb; i++) {
+        dots.array.push(new Dot());
     }
 
-    window.onmousemove = function(parameter) {
-    mousePosition.x = parameter.clientX;
-    mousePosition.y = parameter.clientY;
-}
+    function render() {
 
-    mousePosition.x = window.innerWidth / 2;
-    mousePosition.y = window.innerHeight / 2;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    setInterval(createDots, 1000/30);
+        for (let i = 0; i < dots.nb; i++) {
+            dots.array[i].create();
+        }
+
+        dots.array[0].line();
+        dots.array[0].animate();
+    }
+
+    window.onmousemove = function(e) {
+        mousePosition.x = e.clientX;
+        mousePosition.y = e.clientY;
+    };
+
+    setInterval(render, 1000 / 30);
 };
 
 window.onload = function() {
